7366c454f19ba62ff4cb79fc42d88b57be6e34f8
Brayan-chan <al071392@uacam.mx>
2025-10-20 21:56:36 -0600

    feat: Agregar rutas de saludo de prueba a la aplicación

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import greetingRoutes from './routes/greetingRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api', greetingRoutes);
 
 const PORT = process.env.PORT || 3000;
 
