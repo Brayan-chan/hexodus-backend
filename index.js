3b461d920586a3fcf8b70a25df8a021208deba64
Brayan-chan <al071392@uacam.mx>
2025-10-21 19:04:38 -0600

    feat: Agregar rutas de socios y mejorar la organización del código

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+
+/**
+ * Importamos las rutas
+ */
 import greetingRoutes from './routes/greetingRoutes.js';
 import testRoutes from './routes/testRoutes.js';
+import sociosRoutes from './routes/sociosRoutes.js';
 
-// Cargar variables de entorno al inicio
+/**
+ * Cargar variables de entorno al inicio
+ */
 dotenv.config();
 
-// Verificar que las variables de entorno necesarias estén definidas
+/**
+ * Verificar que las variables de entorno necesarias estén definidas
+ */
 if (!process.env.SUPABASE_KEY) {
   console.error('Error: SUPABASE_KEY no está definida en el archivo .env');
   process.exit(1);
 }
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
+/**
+ * Definimos las rutas a usar
+ */
 app.use('/api', greetingRoutes);
 app.use('/api', testRoutes);
+app.use('/api', sociosRoutes);
 
 const PORT = process.env.PORT || 3000;
 
