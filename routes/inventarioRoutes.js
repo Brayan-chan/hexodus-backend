7e1965e0e832a10bc3bc3a52e7f4ca3529358bb1
Brayan-chan <al071392@uacam.mx>
2025-11-17 20:24:03 -0600

    feat: Eliminar la ruta de inventario ya que no se utiliza

--- routes/inventarioRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createProduct } from '../controllers/inventarioController.js';
-
-const router = express.Router();
-
-// Crear un nuevo producto
-router.post('/inventario', createProduct);
-
-export default router;
