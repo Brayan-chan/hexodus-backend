b01da3938de3369ec93ef83de4a6b061de2afe2a
Brayan-chan <al071392@uacam.mx>
2025-11-17 20:24:10 -0600

    feat: Eliminar la ruta de saludo ya que no se utiliza

--- routes/greetingRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getGreeting } from '../controllers/greetingController.js';
-
-const router = express.Router();
-
-router.get('/greeting', getGreeting);
-
-export default router;
