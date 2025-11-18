50c2a14cbb150875e2d47767d43dae63f37fdcfb
Brayan-chan <al071392@uacam.mx>
2025-11-17 20:23:20 -0600

    feat: Eliminar la ruta de membresías ya que no se utiliza

--- routes/membershipRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getMemberships } from '../controllers/membershipController.js';
-
-const router = express.Router();
-
-router.get('/membresias', getMemberships);
-
-export default router;
