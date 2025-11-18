eea3ec4e1eb470b3a9002d8346bec7c558a056c2
Brayan-chan <al071392@uacam.mx>
2025-11-17 20:23:35 -0600

    feat: Eliminar la ruta de prueba ya que no se utiliza

--- routes/testRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { testConnection } from '../controllers/testController.js';
-
-const router = express.Router();
-
-router.get('/test-connection', testConnection);
-
-export default router;
