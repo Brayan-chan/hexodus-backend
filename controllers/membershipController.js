d80eac3b88780523be4ed8f73a3f2a34827237ab
Brayan-chan <al071392@uacam.mx>
2025-11-17 20:24:29 -0600

    feat: Eliminar el controlador de membresías ya que no se utiliza

--- controllers/membershipController.js
+++
@@ -1,7 +0,0 @@
-export const getMemberships = (req, res) => {
-  res.json({ message: "Devolviendo listado de membresías", memberships: [
-    { id: 1, nombre: "Membresía 1", tipo: "Premium", precio: 100 },
-    { id: 2, nombre: "Membresía 2", tipo: "Básica", precio: 50 },
-    { id: 3, nombre: "Membresía 3", tipo: "VIP", precio: 150 }
-  ]});
-};
