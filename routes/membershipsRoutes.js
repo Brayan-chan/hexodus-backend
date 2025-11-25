408c29a4ebc562ffcaf852abcdd29b3e89258154
Brayan-chan <al071392@uacam.mx>
2025-11-25 01:17:46 -0600

    feat: actualizar rutas de filtrado de membresías y reorganizar el orden

--- routes/membershipsRoutes.js
+++ routes/membershipsRoutes.js
@@ -21,19 +21,22 @@ router.get('/', membershipsController.getMemberships);
 // 5. GET - Buscar membresías
 router.get('/search', membershipsController.searchMemberships);
 
-// 6. GET - Filtrar membresías por status (activo, inactivo)
+// 6. GET - Filtrar membresías con múltiples criterios
+router.get('/filter', membershipsController.filterMemberships);
+
+// 7. GET - Filtrar membresías por status (activo, inactivo)
 router.get('/filter/status', membershipsController.filterMembershipsByStatus);
 
-// 7. GET - Filtrar membresías por tipo (mensual, semanal, etc)
+// 8. GET - Filtrar membresías por tipo (mensual, semanal, etc)
 router.get('/filter/type', membershipsController.filterMembershipsByType);
 
 // 4.1. GET - Obtener detalles de una membresía específica (debe ir al final de las rutas GET)
 router.get('/:membershipId', membershipsController.getMembershipById);
 
-// 8. PUT - Habilitar membresía
+// 9. PUT - Habilitar membresía
 router.put('/:membershipId/enable', membershipsController.enableMembership);
 
-// 9. PUT - Deshabilitar membresía
+// 10. PUT - Deshabilitar membresía
 router.put('/:membershipId/disable', membershipsController.disableMembership);
 
 export default router;
